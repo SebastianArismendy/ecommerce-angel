7e5400ccd16433154acd5a7221d6fe6f74c7f0bd
sebastian.arismendy <sebastian.arismendy@pragma.com.co>
2023-05-30 23:32:14 -0500

    change render app

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
 
     useEffect(() => {
         getResponse();
-        getResponseNode();
+        // getResponseNode();
     }, []);
 
     return (
--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import './index.css';
+import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './GlobalComponents/ThemeProvider';
 import { CartProvider } from 'react-use-cart';
-import { createRoot } from 'react-dom';
+// import { createRoot } from 'react-dom';
 
 
-createRoot(document.getElementById('root')).render(
+render(
   <React.StrictMode>
     <ThemeProvider>
       <CartProvider>
         <App />
       </CartProvider>
     </ThemeProvider>
-  </React.StrictMode>);
+  </React.StrictMode>
+, document.getElementById('root'));
+
+
+
+// createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ThemeProvider>
+//       <CartProvider>
+//         <App />
+//       </CartProvider>
+//     </ThemeProvider>
+//   </React.StrictMode>);
 
 // reportWebVitals();
